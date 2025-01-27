b3cef2b88f66d82ce5b7edb11b954fb0ceced6ee
Henrique Costa <henrique@umai.blue>
2025-01-27 21:53:14 +1100

    init

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter, Lusitana, Zain } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({ subsets: ['latin'], weight: ['400', '700']})
+export const zain = Zain({ subsets: ['latin'], weight: ['400', '700']})
